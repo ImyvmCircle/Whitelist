fccfc188160e5c0fb8443f37be15cb8badb63b20
BuildTools <unconfigured@null.spigotmc.org>
2020-01-18 11:13:28 +0800

    add passed players page

---
+++ handlers/passed.py
@@ -0,0 +1,39 @@
+import logging
+import json
+import os
+
+from handlers.base import BaseHandler
+from handlers.utils import require_login
+from orm.models import Player
+from settings import tornado_settings, smtp_settings, pass_reply
+from email.mime.text import MIMEText
+
+import handlers.mail as mail
+
+
+logger = logging.getLogger('whitelist.' + __name__)
+
+
+class PassedPageHandle(BaseHandler):
+    # @require_login
+    async def get(self):
+        players = await Player.filter(passed=1)
+        await self.render("passed.html", players=players, handler=self)
+
+
+class PassedResultHandle(BaseHandler):
+    @require_login
+    async def post(self):
+        id = int(self.get_argument("id"))
+        result = int(self.get_argument("result"))
+        reason = str(self.get_argument("reason"))
+        assert result in [3]
+
+        player = await Player.get(id=id)
+        player.passed = result
+        await player.save()
+        if reason:
+            cmd = "screen -S BC -p 0 -X stuff \"buc ban "+ player.minecraft_id+" "+reason+"\\r\""
+        else:
+            cmd = "screen -S BC -p 0 -X stuff \"buc ban "+ player.minecraft_id+"\\r\""
+        os.system(cmd)
--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from handlers.apply import ApplyHandle
 from handlers.auth import RegisterHandler, LoginHandler
 from handlers.review import ReviewPageHandle, ReviewResultHandle
+from handlers.passed import PassedPageHandle, PassedResultHandle
 from tornado.web import StaticFileHandler
 
 import settings
@@ -13,7 +14,10 @@ url_patterns = (
     (API_ROOT + r"/auth/register", RegisterHandler),
     (URL_ROOT + r"/login", LoginHandler),
     (URL_ROOT + r"/review", ReviewPageHandle),
+    (URL_ROOT + r"/review", ReviewPageHandle),
     (URL_ROOT + r"/review/set_result", ReviewResultHandle),
+    (URL_ROOT + r"/passed", PassedPageHandle),
+    (URL_ROOT + r"/passed/set_result", PassedResultHandle),
     # (URL_ROOT + r"/review/render/(\d+)", ReviewPieceHandle),
     (URL_ROOT + r"/", ApplyHandle),
     (URL_ROOT + r"/static/(.*)", StaticFileHandler, {'path': settings.tornado_settings['static_path']})
